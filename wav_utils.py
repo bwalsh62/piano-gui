13062372107bfff9433c88528bf9fcd9649fe5c9
Ben Walsh <bwalsh62@gmail.com>
2019-12-23 08:08:15 -0600

    Adding output file name as function argument. Updating comments

--- wav_utils.py
+++ wav_utils.py
@@ -2,12 +2,17 @@
 """
 Created on Mon Oct 21 15:28:27 2019
 
-@author: benja
+@author: Ben Walsh
+For Liloquy
+
+wav_file_clip(time_length, wav_file_input, wav_file_output)
+    time_length = time in seconds to clip .wav file
+    wav_file_input = path to .wav file desired to be shorteneed
+    wav_file_output = new name of .wav file
+    If no wav_file_output, assumed <wav_file_name>_clipped.wav
 
 # TO DO
 - add error checking to wav_file_clip
-- add wav util to combine two files
-- use wav_file_combine to generate chord progression
 
 """
 #%% Import libraries
@@ -16,19 +21,22 @@ import wave
 from os.path import exists
 
 #%% Shorten input wav file
-def wav_file_clip(t_len,wav_file_in):
+def wav_file_clip(t_len,wav_file_in,wav_file_out=None):
     
+    # If no output name, supplied, use input name with _clipped suffix
+    if wav_file_out==None:
+        wav_file_out = wav_file_in[:-4]+'_clipped.wav'
+        
     # ADD error checking to wav_file_in existence
+    # Right now prints message but doesn't do anything
     if not(exists(wav_file_in)):
         print('Input file does not exist:'+wav_file_in)
-    
-    # ADD error checking that t_len is shorter than actual time length
-    
+        
     # Open wav file to read
     obj = wave.open(wav_file_in,'r')
 
     # Get sampling rate
-    fs = obj.getframerate() # should get from audio
+    fs = obj.getframerate() 
     # Get other parameters to replicate in new file
     n_channels = obj.getnchannels()
     samp_width = obj.getsampwidth()
@@ -36,11 +44,12 @@ def wav_file_clip(t_len,wav_file_in):
     # Based on fs, get number of frames for input time length
     n_frames = int(fs*t_len)
     
+    # ADD error checking that t_len is shorter than actual time length
+    # Right now prints message but doesn't do anything
     if n_frames > obj.getnframes():
         print('Desired output time length is longer than wav file length')
         # Could add 0s instead
         
-    
     ## For debugging
 #    print( "Number of channels",obj.getnchannels())
 #    print ( "Sample width",obj.getsampwidth())
@@ -50,23 +59,16 @@ def wav_file_clip(t_len,wav_file_in):
     frames_read = obj.readframes(n_frames)
     obj.close()
     
-    # Write new file
-    #---------------
-    # Declare new file name with suffix
-    new_fname = wav_file_in[:-4]+'_clipped.wav'
-    
     # Open new wav file
-    obj = wave.open(new_fname,'wb')
+    obj = wave.open(wav_file_out,'wb')
     obj.setnframes(n_frames)
     obj.setnchannels(n_channels)
     obj.setsampwidth(samp_width)
     obj.setframerate(fs)
-    obj.writeframes(frames_read) #or writeframesraw?
+    obj.writeframes(frames_read) 
     obj.close()
-    
-    #print('Created: '+new_fname)
-    
-    return new_fname
+        
+    return wav_file_out
 
 #%% Append two wav files into new file
 
@@ -99,10 +101,8 @@ def wav_file_append(wav_file1,wav_file2,merge_name='./appended.wav'):
     obj.setnchannels(n_channels)
     obj.setsampwidth(samp_width)
     obj.setframerate(fs)
-    obj.writeframes(wav1_data+wav2_data) #or writeframesraw?
+    obj.writeframes(wav1_data+wav2_data) 
     obj.close()
-    
-    #print('Created: '+merge_name)
-    
+        
     return merge_name
 
