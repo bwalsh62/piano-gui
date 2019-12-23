6a1794c22b771032eb9f1bdc1f2ee173953e0df4
Ben Walsh <bwalsh62@gmail.com>
2019-12-23 08:09:46 -0600

    Adding note_name mode to make_melody so strings of notes C4-D4 etc. can create melodies, as output from ML model

--- melody.py
+++ melody.py
@@ -19,37 +19,72 @@ melody_wav = make_melody('./outputmelody.wav',0,5,7,0)
 # TO DO
 - Fix why note_repeats=2 repeats to 4
 --  Doubling the note lengths, 1-2-4 etc. not actually repeating
-- To fix above, can update wav_file_clip to have optional arg for name/suffix
-   then initialize a separate note_array for repeated notes
 
 """
 
 #%% Import custom libraries
-from piano_notes import sound_paths #, sound_dict
+from piano_notes import sound_paths, sound_path_dict
 
 # Custom wav_file helper functions
 from wav_utils import wav_file_append, wav_file_clip
 
 #%% Make melody wav file from input notes
 
-def make_melody(mel_wav_name,note_array,bpm=60):
+def make_melody(mel_wav_name,note_array,bpm=60,mode="note_idx",vol_const=1,debug=0):
         
     t_len = 60/bpm # time in seconds
     note_repeats = 2 # assume 2 for now, can be input or based on bpm and 4/4
     
-    for idx, note_num, in enumerate(note_array):
-        # Path of corresponding wav file
-        note_wav_file = sound_paths[note_num]
-        # Generate wav file and return new name
-        note_wav_file = wav_file_clip(t_len,note_wav_file)
-        # If notes are repeated, append to existing wav file
-        # Below keeps doubling the note lengths, 1-2-4 etc. not actually repeating
-        for repeat in range(1):
-            wav_file_append(note_wav_file,note_wav_file,note_wav_file)
-        if idx==0:
-            # Initalize melody name
-            mel_wav_name = wav_file_clip(t_len*note_repeats,note_wav_file)
-        elif idx>0:
-            wav_file_append(mel_wav_name,note_wav_file,mel_wav_name)
-        
+    if debug:
+        print('Make melody, mode={}'.format(mode))
+        print('note_array={}'.format(note_array))
+        print('mel_wav_name={}'.format(mel_wav_name))
+    
+    # Allow inputs of 'C4' 'D4' etc.
+    #---------------------------
+    if mode=="note_name":
+        for idx, note in enumerate(note_array):
+            # Get note .wav file path
+            note_wav_file = sound_path_dict[note]
+            # Return new file path of _clipped .wav file
+            note_wav_file = wav_file_clip(t_len,note_wav_file)
+            if debug:
+                print('note_wav_file = {}'.format(note_wav_file))
+                print('idx = {}'.format(idx))
+                print('note = {}'.format(note))
+
+            # If notes are repeated, append to existing wav file
+            # Below keeps doubling the note lengths, 1-2-4 etc. not actually repeating
+            for repeat in range(1):
+                # Repeat note
+                # This should be to a new note, not overwriting the same note 
+                wav_file_append(note_wav_file,note_wav_file,note_wav_file)
+            if idx==0:
+                # Initalize melody name
+                mel_wav_name = wav_file_clip(t_len*note_repeats,note_wav_file,mel_wav_name)
+            elif idx>0:
+                wav_file_append(mel_wav_name,note_wav_file,mel_wav_name)
+    elif mode=="note_idx": 
+        for idx, note_num, in enumerate(note_array):
+            # Path of corresponding wav file
+            note_wav_file = sound_paths[note_num]
+            # Generate wav file and return new name
+            note_wav_file = wav_file_clip(t_len,note_wav_file)
+            if debug:
+                print('note_wav_file = {}'.format(note_wav_file))
+                print('idx = {}'.format(idx))
+                print('note_num = {}'.format(note_num))
+                
+            # If notes are repeated, append to existing wav file
+            # Below keeps doubling the note lengths, 1-2-4 etc. not actually repeating
+            for repeat in range(1):
+                wav_file_append(note_wav_file,note_wav_file,note_wav_file)
+            if idx==0:
+                # Initalize melody name
+                mel_wav_name = wav_file_clip(t_len*note_repeats,note_wav_file,mel_wav_name)
+            elif idx>0:
+                wav_file_append(mel_wav_name,note_wav_file,mel_wav_name)
+    else:
+        print("Mode {} for melody.py not recognized".format(mode))
+    
     return mel_wav_name
